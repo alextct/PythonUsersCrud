3877eb5503794cd84ad08e99e4b52824b078b8e3
alex <alex@tct.travel>
2023-08-13 21:52:08 +0300

    am facut getul

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, request
+from app.models.user import User
 
 app = Flask(__name__)
 
@@ -7,14 +8,21 @@ app = Flask(__name__)
 @app.route('/data/<user_id>', methods=['GET', 'POST', 'DELETE', 'PUT'])
 def user(user_id):
     if request.method == 'GET':
-        return {'user_id': user_id}
+        response =  User.get_by_id(user_id)
+        if response:
+            return response
+        return {"message": "User not found in DB"}
 
     elif request.method == 'POST':
         # getting the json data payload from request
         request_data = request.json
         # treating request_data as a dictionary to get a specific value from key
         user_name = request_data.get('user_name')
-        return {'user id': user_id, 'user name': user_name}
+        user = User(None, user_name)
+        response = User.save()
+        return response
+
+        # return {'user id': user_id, 'user name': user_name}
 
 
 # todo elif for put and delete
