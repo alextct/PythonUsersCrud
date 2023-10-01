16a1e71fb3984e806987932b27822498ae9e8253
Alexandru Elisei <alex@tct.travel>
2023-10-01 21:48:08 +0300

    check

--- clean_environment.py
+++ clean_environment.py
@@ -1,7 +1,7 @@
 import requests
 
 try:
-    requests.get("http://127.0.0.1:5000/stop_server")
+    print(requests.get("http://127.0.0.1:5000/stop_server"))
 except Exception as e:
     print("An error occurred:", e)
 finally:
